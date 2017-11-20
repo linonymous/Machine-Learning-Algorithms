f358e4badbf568c0ede7bfe960a3d881c2cfbb20
Swapnil Walke <iamlinonymous@gmail.com>
2017-11-20 22:43:35 +0530

    add gradient step

---
+++ Multivariate-Linear-Regression/gradient_step.py
@@ -0,0 +1,10 @@
+from hypothesis import hypothesis
+
+def gradient_step(input_x, output_y, theta, learning_rate):
+    t_theta = theta
+    for i in len(theta):
+        if i == 0:
+            theta[i] = t_theta - learning_rate * (hypothesis(input_x, t_theta) - output_y)
+        else:
+            theta[i] = t_theta - learning_rate * (hypothesis(input_x, t_theta) - output_y) * input_x[i]
+    return theta
