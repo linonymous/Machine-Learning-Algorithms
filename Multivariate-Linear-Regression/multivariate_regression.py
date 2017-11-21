fd53eb7e833c94772e5c281578e70297282b24c5
Swapnil.Walke <iamlinonymous@gmail.com>
2017-11-21 08:39:10 +0530

    - gardient_runner add

--- Multivariate-Linear-Regression/multivariate_regression.py
+++ Multivariate-Linear-Regression/multivariate_regression.py
@@ -6,3 +6,9 @@ import numpy as np
 def init_theta(num_features):
     list_of_zeroes = [0] * num_features
     return list_of_zeroes
+
+def gradient_descent_runner(input_x, output_y, theta, num_iterations, learning_rate):
+    for i in range(num_iterations):
+        for index in len(output_y):
+            theta = gradient_step(input_x[index], output_y[index], theta, learning_rate)
+    return theta
