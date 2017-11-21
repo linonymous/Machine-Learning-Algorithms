1e801d216865837d41cef4bc9c7f721779cd6c6e
Swapnil.Walke <iamlinonymous@gmail.com>
2017-11-21 08:00:56 +0530

    debug the index error

--- Multivariate-Linear-Regression/gradient_step.py
+++ Multivariate-Linear-Regression/gradient_step.py
@@ -4,7 +4,7 @@ def gradient_step(input_x, output_y, theta, learning_rate):
     t_theta = theta
     for i in len(theta):
         if i == 0:
-            theta[i] = t_theta - learning_rate * (hypothesis(input_x, t_theta) - output_y)
+            theta[i] = t_theta[i] - learning_rate * (hypothesis(input_x, t_theta) - output_y)
         else:
-            theta[i] = t_theta - learning_rate * (hypothesis(input_x, t_theta) - output_y) * input_x[i]
+            theta[i] = t_theta[i] - learning_rate * (hypothesis(input_x, t_theta) - output_y) * input_x[i]
     return theta
