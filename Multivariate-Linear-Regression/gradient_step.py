ff8ba9b602c618c8e528cecacc9ce027c7436130
Swapnil.Walke <iamlinonymous@gmail.com>
2017-11-22 22:45:50 +0530

    - correction

--- Multivariate-Linear-Regression/gradient_step.py
+++ Multivariate-Linear-Regression/gradient_step.py
@@ -1,10 +1,7 @@
 from hypothesis import hypothesis
-
-def gradient_step(input_x, output_y, theta, learning_rate):
+import numpy as np
+def gradient_step(input_x, output_y, theta, learning_rate, data_size):
     t_theta = theta
-    for i in len(theta):
-        if i == 0:
-            theta[i] = t_theta[i] - learning_rate * (hypothesis(input_x, t_theta) - output_y)
-        else:
-            theta[i] = t_theta[i] - learning_rate * (hypothesis(input_x, t_theta) - output_y) * input_x[i]
+    for i in range(len(theta)):
+        theta[i] = t_theta[i] - learning_rate * 1/data_size  *(hypothesis(input_x, t_theta) - output_y) * input_x[i]
     return theta
