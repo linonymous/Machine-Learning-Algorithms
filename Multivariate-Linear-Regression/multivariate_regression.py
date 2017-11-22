7c036398f7e7b5eae7b07e2304efc153ef9500c2
Swapnil.Walke <iamlinonymous@gmail.com>
2017-11-22 22:46:32 +0530

    - added call to gradient_descent_runner

--- Multivariate-Linear-Regression/multivariate_regression.py
+++ Multivariate-Linear-Regression/multivariate_regression.py
@@ -9,6 +9,20 @@ def init_theta(num_features):
 
 def gradient_descent_runner(input_x, output_y, theta, num_iterations, learning_rate):
     for i in range(num_iterations):
-        for index in len(output_y):
-            theta = gradient_step(input_x[index], output_y[index], theta, learning_rate)
+        for index in range(len(output_y)):
+            data = np.array([1, input_x[index]])
+            theta = gradient_step(data, output_y[index], theta, learning_rate, len(output_y))
     return theta
+
+data = np.loadtxt("train.csv",delimiter=",")
+input_x = data[:,0]
+input_y = data[:,1]
+theta = init_theta(2)
+theta = gradient_descent_runner(input_x, input_y, theta, num_iterations=2000, learning_rate=0.001)
+print theta
+
+test = np.loadtxt("train.csv",delimiter=",")
+input_x = data[:,0]
+input_y = data[:,1]
+print np.array([1, input_x[0]])
+print hypothesis(np.array([1, input_x[0]]),theta)
